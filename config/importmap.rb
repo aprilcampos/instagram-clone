41cf4007daa94e8b2a40011a332a5d4390d3d457
April Camille Campos <aprilcamille.campos@gmail.com>
2025-04-04 11:40:05 +0800

    fixing popperjs error to display dropdown values

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application"
 pin "bootstrap" # @5.3.4
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
